88b15d7915c56a90ec433bf6d6120c9131e4457e
Cyril Gourdon <cyril.gourdon.02@gmail.com>
2021-12-22 15:52:54 +0100

    introduction des sous menu sous forme de const

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Liens from "./pages/Liens";
 import Presentation from "./pages/Presentation";
-import Scripts from "./pages/Scripts";
+import {Script1,Script2,Script3,Script4} from "./pages/Scripts";
 import Studi from "./pages/Studi";
 import Cybersecurite from "./pages/Cybersecurite";
 
@@ -16,10 +16,15 @@ const App = () => {
       <Route path="/contact" element={<Contact/>}/>
       <Route path="/cybersecurite" element={<Cybersecurite/>}/>
       <Route path="/presentation" element={<Presentation/>}/>
-      <Route path="/scripts" element={<Scripts/>}/>
+      <Route path="/script-1" element={<Script1/>}/>
+      <Route path="/script-2" element={<Script2/>}/>
+      <Route path="/script-3" element={<Script3/>}/>
+      <Route path="/script-4" element={<Script4/>}/>
+
+
       <Route path="/studi" element={<Studi/>}/>
       <Route path="/liens" element={<Liens/>}/>
-
+      
       
     </Routes>
   );
